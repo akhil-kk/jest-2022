41c74ec15e3998c16803075d10eb799b61915ffb
AKHIL K K <akhilkkopparambil@gmail.com>
2022-03-08 10:58:00 +0530

    queryby methods in tetsing

--- react-testing-library/src/components/Header/__test__/Header.test.js
+++ react-testing-library/src/components/Header/__test__/Header.test.js
@@ -7,20 +7,36 @@ it('should render same text passd into title prop', async () => {
   expect(headingElement).toBeInTheDocument()
 })
 
-it('should render same heading', async () => {
-  render(<Header title="My header" />)
-  const headingElement = screen.getByRole('heading')
-  expect(headingElement).toBeInTheDocument()
-})
+// it('should render same heading', async () => {
+//   render(<Header title="My header" />)
+//   const headingElement = screen.getByRole('heading')
+//   expect(headingElement).toBeInTheDocument()
+// })
 
-it('should render same text id', async () => {
-  render(<Header title="My header" />)
-  const headingElement = screen.getByTestId('header-1')
-  expect(headingElement).toBeInTheDocument()
-})
+// it('should render same text id', async () => {
+//   render(<Header title="My header" />)
+//   const headingElement = screen.getByTestId('header-1')
+//   expect(headingElement).toBeInTheDocument()
+// })
 
-it('should render same text', async () => {
-  render(<Header title="My header" />)
-  const headingElement = await screen.findByText(/My header/)
-  expect(headingElement).toBeInTheDocument()
-})
+// //findby
+
+// it('should render same text', async () => {
+//   render(<Header title="My header" />)
+//   const headingElement = await screen.findByText(/My header/)
+//   expect(headingElement).toBeInTheDocument()
+// })
+
+// //query by
+
+// it('should query same text', async () => {
+//   render(<Header title="My header" />)
+//   const headingElement = screen.queryByText(/dogs/i)
+//   expect(headingElement).not.toBeInTheDocument()
+// })
+
+// it('should render the same text pased', async () => {
+//   render(<Header title="My header" />)
+//   const headingElement = screen.getAllByRole('heading')
+//   expect(headingElement.length).toBe(2)
+// })
